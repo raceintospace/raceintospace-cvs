3a9edc14929c26528754812b5227a8e73168e368
k6mmc <k6mmc>
2007-01-09 15:31:57 +0000

    Moved things from pace.c into endianness.c

--- pace.c
+++ pace.c
@@ -846,19 +846,6 @@ dbg (char const *fmt, ...)
 	va_end (args);
 }
 
-#if defined(MACOSX) && defined(__POWERPC__)
-ui32 _SwapLong(ui32 value)
-{
-      return (( value >> 24) | ((value >> 8) & 0x0000FF00) | ((value << 8) & 0x00FF0000) | (value << 24));
-}
-
-ui16 _SwapWord(ui16 value)
-{
-      return ((value << 8) | (value >> 8));
-}
-#endif
-
-
 #ifdef _WIN32
 #include <sys/timeb.h>
 int
--- pace.h
+++ pace.h
@@ -196,10 +196,12 @@ void dbg (char const *fmt, ...) __attribute__ ((format (printf, 1, 2)));
 ui32 _SwapLong(ui32 value);
 ui16 _SwapWord(ui16 value);
 void SwapGameDat(void);
+void SwapPal(char *pal);
 #else
-#define SwapLong(a)
-#define SwapWord(a)
+#define SwapLong(a) (a)
+#define SwapWord(a) (a)
 #define SwapGameDat()
+#define SwapPal(a)
 #endif
 
 
