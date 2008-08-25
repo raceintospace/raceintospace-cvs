d2537897c00da2afef28e156bf7336062da31511
k6mmc <k6mmc>
2008-08-25 17:39:27 +0000

    2008-08-25	Michael McCarty <mkmccarty@raceintospace.org> 	* V215 	* DEVELOPER: added additional OSX notes 	* os_macosx: Added libraries to Xcode project

--- os_macosx/SDLMain.m
+++ os_macosx/SDLMain.m
@@ -177,13 +177,13 @@ static BOOL   gCalledAppMainline = FALSE;
 #endif
 
 /* Main entry point to executable - should *not* be SDL_main! */
-int main (int argc, char **argv)
+int main (int argc,  const char *argv[])
 {
     /* Copy the arguments into a global variable */
     /* This is passed if we are launched by double-clicking */
     if ( argc >= 2 && strncmp (argv[1], "-psn", 4) == 0 ) {
         gArgv = (char **) SDL_malloc(sizeof (char *) * 2);
-        gArgv[0] = argv[0];
+        gArgv[0] = (char *)argv[0];
         gArgv[1] = NULL;
         gArgc = 1;
         gFinderLaunch = YES;
@@ -192,7 +192,7 @@ int main (int argc, char **argv)
         gArgc = argc;
         gArgv = (char **) SDL_malloc(sizeof (char *) * (argc+1));
         for (i = 0; i <= argc; i++)
-            gArgv[i] = argv[i];
+            gArgv[i] = (char *)argv[i];
         gFinderLaunch = NO;
     }
 
